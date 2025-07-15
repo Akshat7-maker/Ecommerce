d197f1aa5c3def657d212796af311724b0423d82
Akshat Chopra <chopraakshat62@gmail.com>
2025-07-15 05:58:03 +0530

    changed url

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const API = axios.create({
-    baseURL:"http://localhost:8000/api/v1",
+    baseURL:"https://ecommerce-k2yn.onrender.com",
     withCredentials: true
 });
 
--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,7 @@ import path from "path"
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:8000", "http://localhost:5173"],
+    origin: ["http://localhost:8000", "http://localhost:5173","https://ecommerce-k2yn.onrender.com"],
     credentials: true  // allows cookies to be sent
 }))
 
